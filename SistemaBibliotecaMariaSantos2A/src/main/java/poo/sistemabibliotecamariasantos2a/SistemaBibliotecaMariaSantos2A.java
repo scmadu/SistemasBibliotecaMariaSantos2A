a185ebeb836cb5475c10228f83827d2feaecf076
scmadu <eduardamaria51903@gmail.com>
2025-04-22 11:29:51 -0400

    Add files via upload

---
+++ SistemaBibliotecaMariaSantos2A/src/main/java/poo/sistemabibliotecamariasantos2a/SistemaBibliotecaMariaSantos2A.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.sistemabibliotecamariasantos2a;
+
+/**
+ *
+ * @author 2024105070020
+ */
+public class SistemaBibliotecaMariaSantos2A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
